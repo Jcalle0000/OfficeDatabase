d3858447619d2f2c1db95c2cbfda6879fbd5ac6c
Jason <jcalle0000@gmail.com>
2020-10-27 19:28:50 -0400

    Added more tables and more relationships

--- Insert.sql
+++ Insert.sql
@@ -5,7 +5,7 @@ VALUES ('1', 'Male', '01-02-2000', '2000 3rd ave',
         '10021', 'New York', 'Jay', 'Cal'   )
 
 
--- adding a department 
+-- First Department created is for software
 INSERT INTO DEPARTMENT (department_id, dname ) 
 VALUES ( '001', 'Software' )
 
@@ -13,3 +13,10 @@ VALUES ( '001', 'Software' )
 INSERT INTO EMPLOYEE 
 VALUES ('2', 'Male', '03-02-2001', '2001 4th ave', 
         '10022', 'New York', 'Ron', 'Cane', '001'   )
+
+
+-- Employee 3 will work in software (last column is 001)
+        -- since various employees can be working for a department 
+INSERT INTO EMPLOYEE 
+VALUES ('3', 'Male', '09-20-1995', '5642 2nd ave', 
+        '14411', 'New York', 'Victor', 'German', '001'   )
--- create.sql
+++ create.sql
@@ -29,10 +29,10 @@ CREATE TABLE EMPLOYEE (
 CREATE table DEPARTMENT (
     department_id    int             not null,
     dname            VARCHAR(10),
-    startDate        VARCHAR(15),
+    startDate        VARCHAR(15),  -- this column was supposed to be used for when an employee starts managing a department 
 
-    employee_id int FOREIGN KEY REFERENCES EMPLOYEE(employee_id)   , 
-    location_id int FOREIGN KEY REFERENCES LOCATIONS (location_id),
+    employee_id int FOREIGN KEY REFERENCES EMPLOYEE(employee_id)   ,  -- can have various employees working for the department 
+    location_id int FOREIGN KEY REFERENCES LOCATIONS (location_id), -- can have multiple locations 
 
     PRIMARY KEY (department_id)
 )
@@ -42,4 +42,35 @@ alter table EMPLOYEE add department_id int;
 
 alter TABLE EMPLOYEE 
 ADD FOREIGN KEY (department_id) REFERENCES DEPARTMENT(department_id);
+
+-- we are not making department_id requied
+    -- b/c we may want to add a project and may 
+    -- not be sure of what department we want to add it to
+    -- Incorporates relationship 4
+CREATE TABLE PROJECT (
+    project_id  INT          NOT NULL ,    
+    projName    VARCHAR(20),
+    department_id INT       , -- we are not specifying as null
+
+    FOREIGN KEY (department_id) REFERENCES DEPARTMENT (department_id),
+
+    PRIMARY KEY(project_id)
+)
+
+-- this is intresting as it has two primary keys
+    -- since theyre primary keys they cannot be empty
+-- M:N Relationship
+-- Relationship 3
+CREATE TABLE WORKS_ON(
+    project_id      INT      NOT null, 
+    employee_id     INT      not NULL,
+    FOREIGN KEY   (project_id) REFERENCES PROJECT(project_id),
+    FOREIGN KEY   (employee_id) REFERENCES EMPLOYEE (employee_id),
+
+    amount_of_Hours INT,           
+
+    -- constraints are needed for multiple primary keys?
+    CONSTRAINT PK_WORKS_ON PRIMARY KEY (project_id, employee_id)
+    
+)
 
